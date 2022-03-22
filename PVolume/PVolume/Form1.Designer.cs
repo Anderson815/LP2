23e23e74912cb3e6144720518072e214aee08cc6
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2022-03-22 10:46:47 -0300

    correção do exercício 1 - PVolume

---
+++ PVolume/PVolume/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PVolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Raio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Volume";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Altura";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(49, 235);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(145, 60);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(221, 235);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(145, 60);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(177, 52);
+            this.txtRaio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(188, 22);
+            this.txtRaio.TabIndex = 5;
+            this.txtRaio.Validated += new System.EventHandler(this.txtRaio_Validated);
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Enabled = false;
+            this.txtVolume.Location = new System.Drawing.Point(177, 154);
+            this.txtVolume.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(188, 22);
+            this.txtVolume.TabIndex = 6;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(177, 101);
+            this.txtAltura.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(188, 22);
+            this.txtAltura.TabIndex = 7;
+            this.txtAltura.Validated += new System.EventHandler(this.txtAltura_Validated);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 335);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Calculo do Volume do Cilíndro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.TextBox txtAltura;
+    }
+}
+
---
+++ PVolume/PVolume/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PVolume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Validação
+            Double raio, altura, volume;
+
+            do
+            {
+                raio = this.ValidacaoRaio();
+                if (raio == -1) txtRaio.Focus();
+            } while (true);
+            altura = this.ValidacaoAltura();
+            
+            
+
+            volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            txtVolume.Text = volume.ToString("N2");
+        }
+
+        private void txtRaio_Validated(object sender, EventArgs e)
+        {
+            this.ValidacaoRaio();
+        }
+
+        private void txtAltura_Validated(object sender, EventArgs e)
+        {
+            this.ValidacaoAltura();
+        }
+
+        //Auxiliares
+
+        private Double ValidacaoRaio() 
+        {
+            Double raio;
+
+            if (!Double.TryParse(txtRaio.Text, out raio))
+            {
+                MessageBox.Show("Raio inválido!");
+                raio = -1;
+            }
+            else
+            {
+                if (raio <= 0)
+                {
+                    MessageBox.Show("Raio deve ser maior que zero!");
+                    raio = -1;
+                }
+            }
+
+            return raio;
+        }
+
+        private Double ValidacaoAltura() {
+            Double altura;
+
+            if (!Double.TryParse(txtAltura.Text, out altura))
+            {
+                MessageBox.Show("Altura inválido!");
+            }
+            else
+            {
+                if (altura <= 0)
+                {
+                    MessageBox.Show("Altura deve ser maior que zero!");
+                }
+            }
+
+            return altura;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
